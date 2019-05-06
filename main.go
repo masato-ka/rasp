106e524c16adb2411ba62f947472c73bafe966eb
masato-ka <jp6uzv@gmail.com>
2019-05-06 19:07:23 +0900

    Add version number in circleci setting.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"github.com/google/subcommands"
 	"os"
 	"rasp/cmd/build"
@@ -10,12 +11,21 @@ import (
 
 func main() {
 
+	version := "0.1.3"
+
 	subcommands.Register(subcommands.HelpCommand(), "")
 	//subcommands.Register(subcommands.FlagsCommand(), "")
 	subcommands.Register(subcommands.CommandsCommand(), "")
 	subcommands.Register(&build.BuildCmd{}, "")
 
+	v := flag.Bool("version", false, "version")
+
 	flag.Parse()
+	if *v {
+		fmt.Fprintf(os.Stdout, "rasp %s \n", version)
+		os.Exit(0)
+	}
+
 	ctx := context.Background()
 	os.Exit(int(subcommands.Execute(ctx)))
 }
